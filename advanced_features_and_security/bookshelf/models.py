4633316e47e2053e432346b78287d4449de01168
Blessing Denis <fargoxyt@gmail.com>
2024-11-13 11:58:39 +0100

    made changes

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -1,7 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_year = models.IntegerField(default=2000)
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=true)
+    profile_photo = models.ImageField(blank=true)
+    objects = CustomUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, date_of_birth=None, profile_photo=None):
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth= date_of_birth,
+            profile_photo= profile_photo
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.save(self._db)
+        return user
--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -24,23 +24,3 @@ class Librarian(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=50, choices=[('Admin','Admin'), ('Librarian', 'Librarian'), ('Member', 'Member')])
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(blank=true)
-    profile_photo = models.ImageField(blank=true)
-    objects = CustomUserManager
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, date_of_birth=None, profile_photo=None):
-        user = self.model(
-            email=self.normalize_email(email),
-            date_of_birth= date_of_birth,
-            profile_photo= profile_photo
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    def create_superuser(self, email, password=None):
-        user = self.create_user(email, password=password)
-        user.is_admin = True
-        user.save(self._db)
-        return user
