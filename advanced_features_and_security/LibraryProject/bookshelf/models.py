3fd8fda2c375561ee712a37fa2ccf25a71f962ed
Blessing Denis <fargoxyt@gmail.com>
2024-11-13 12:10:10 +0100

    made changes

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,17 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_year = models.IntegerField(default=2000)
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(blank=true)
-    profile_photo = models.ImageField(blank=true)
-    objects = CustomUserManager
-
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, date_of_birth=None, profile_photo=None):
         user = self.model(
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
 class Author(models.Model):
