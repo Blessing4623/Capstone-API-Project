9b634ca5eadcde1ddf18502f9d1183fff149299e
Blessing Denis <fargoxyt@gmail.com>
2024-11-13 14:35:54 +0100

    made changes

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def EditView(request):
+    return HttpResponse('you can edit')
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    return HttpResponse('you can view')
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def CreateView(request):
+    return HttpResponse('you can create')
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def DeleteView(request):
+    return HttpResponse('you can delete')
