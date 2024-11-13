8d4bf6f052b125e71d382e456b382203cbb9d636
Blessing Denis <fargoxyt@gmail.com>
2024-11-13 12:20:02 +0100

    made changes

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser
+from .admin import CustomAdmin
 # Register your models here.
 admin.site.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('title', 'author')
+
+admin.site.register(CustomUser, CustomAdmin)
