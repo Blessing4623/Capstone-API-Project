fde496f8eaf74f8078f1106a793280718e939507
Blessing Denis <fargoxyt@gmail.com>
2024-11-21 14:30:30 +0100

    made changes

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 # creating a url pattern for the api
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all' )
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('api/token/', obtain_auth_token, name='api_token_auth')
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 # Create your views here.
 class BookList(generics.ListAPIView):
+    authentication_classes= [TokenAuthentication]
+    permission_classes = ['IsAuthenticated']
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    def create(self, serializer):
+        serializer.save(author=self.request.user)
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
