45ae0bacde6ab1d525039212ad20e23d3eabf124
Blessing Denis <fargoxyt@gmail.com>
2024-11-27 18:26:43 +0100

    made changes

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('books/', views.ListView.as_view(), name='book_list'),
     path('books/<int:pk>/', views.DetailView.as_view(), name='book_detail'),
-    path('books/new/', views.CreateView.as_view(), name='book_create'),
-    path('books/<int:pk>/edit', views.UpdateView.as_view(), name='book_update'),
-    path('books/<int:pk>/delete', views.DeleteView.as_view(), name='book_delete')
+    path('books/create/', views.CreateView.as_view(), name='book_create'),
+    path('books/update/', views.UpdateView.as_view(), name='book_update'),
+    path('books/delete/', views.DeleteView.as_view(), name='book_delete')
 ]
