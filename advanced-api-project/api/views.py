b3a4df04faea4e4f4bc6907cc72aca1554ad0286
Blessing Denis <fargoxyt@gmail.com>
2024-11-28 17:00:33 +0100

    made changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,19 +4,16 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 class ListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
     search_fields = ['title', 'author']
-    ordering_fields = "__all__"
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        title = self.request.query_params.get('title')
-        if title:
-            return Book.objects.filter(title=title)
+    filterset_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
