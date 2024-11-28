0fd599dd405145230c63aeaaf7b79fcf410bc7a2
Blessing Denis <fargoxyt@gmail.com>
2024-11-28 16:42:41 +0100

    made changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,11 +3,20 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
 # Create your views here.
 class ListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ['title', 'author']
+    ordering_fields = "__all__"
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.query_params.get('title')
+        if title:
+            return Book.objects.filter(title=title)
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
