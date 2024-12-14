f944fac5f5fa4d4918f3b1d969bc55e59a8e2293
Blessing Denis <fargoxyt@gmail.com>
2024-12-14 16:43:03 +0100

    made changes

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'PASSWORD': 'Baldwin90#'
     }
 }
 
