6e2e189e7fa1948a0f54c71aab91bdc9cbcfd99c
Blessing Denis <fargoxyt@gmail.com>
2024-12-14 14:50:27 +0100

    made changes

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from .views import MovieViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet, basename='movies')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
---
+++ authentication_setup.md
@@ -0,0 +1,3 @@
+## I implemented Json web token authentication
+I configured it in the settings.py where I added JWT to installed apps
+You can try to obtain the token using the url link  after running the development server
